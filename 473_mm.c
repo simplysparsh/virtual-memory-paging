fe866c6c8882cc6ec34c8d15811344f02528f840
Shawn <ssv5039@users.noreply.github.com>
2016-12-01 00:01:49 -0500

    Add files via upload
    Not too much has been added so far. Created Global variables and struct. Starting working on set init and singal_handler function

--- 473_mm.c
+++ 473_mm.c
@@ -3,8 +3,8 @@
 // File             : 473_mm.c
 // Description      : Construct and compare two page replacement algorithms to manage the limited physical memory
 //
-// Author           : @author
-// Last Modified    : @date
+// Author           : Sparsh Saxena and Shawn Varughese
+// Last Modified    : 12/08/2016
 //
 
 // Headers
@@ -17,20 +17,57 @@
 // Macros
 #define FIFO    1
 #define CLOCK   2
+
+// Structures
+typedef struct {
+	int region;
+} virtMem;
+
+typedef struct {
+	int flag; //to check if it is empty or not
+	void *start_mem;
+	void *end_mem;
+	struct myNode *next;
+	struct myNode *previous;
+} myNode;
+myNode nodee;
+
+// Global Variables
+int myType;
+int pageSize; // bytes in a page
+//// if not in mem then page fault
+
+
+void mySigHandler()
+{
+	int *ptr;
+	
+	pageSize = getpagesize(); // returns # of bytes in a page
+	//pageNumber = (si_addr - ptr)/pageSize
+	int pageNumber = (si_addr - ptr)/pageSize;
+}
+
+// Functions to Implement
 unsigned long mm_nsigsegvs()
 {
-/**
- * Fill Your Code Here
- *
- */
+	int i;
+
 	return 0l;
 }
-void mm_init( void* vm, int vm_size, int n_frames, int page_size , int policy )
+
+void mm_init(void* vm, int vm_size, int n_frames, int page_size , int policy)
 {
-/**
- * Fill Your Code Here
- *
- */
+	myType = policy;
+	//int mprotect(void *addr, size_t len, int prot);
+	mprotect(vm, page_size, 0); // PROT_NONE for right
+	// now do sigaction
+	//int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact);
+	sigaction( , , mySigHandler());
+
+	if (myType == 1) // FIFO
+	{
+		
+	}
 }
 
 int mm_report_npage_evicts(int i)
@@ -41,6 +78,7 @@ int mm_report_npage_evicts(int i)
  */
 	return -1;
 }
+
 int mm_report_nframe_evicts(int i)
 {
 /**
@@ -49,8 +87,6 @@ int mm_report_nframe_evicts(int i)
  */
 	return -1;
 }
-
-
 
 unsigned long mm_report_npage_faults()
 {
@@ -69,3 +105,4 @@ unsigned long mm_report_nwrite_backs()
  */
 	return 0l;
 }
+
