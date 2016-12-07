bb2a60238f2b4449113b682d84a2c045df7f17bf
Shawn <ssv5039@users.noreply.github.com>
2016-12-07 15:19:04 -0500

    Creating FIFO in Handler Function

--- 473_mm.c
+++ 473_mm.c
@@ -20,13 +20,13 @@
 
 // Structures
 typedef struct {
-	int size;
-	void *start_mem;
-	void *end_mem;
-	struct myNode *next;
-	struct myNode *previous;
-} myNode;
-myNode nodee;
+	int pageNumber;
+	void *startOfMem;
+	int dirtyBit;
+	int listLength; // to check if full or not
+	struct pageNode *next;
+} pageNode;
+pageNode nodee;
 
 // sigaction struct in 'signal.h'
 /*struct sigaction{
@@ -39,12 +39,13 @@ struct sigaction sa; // using struct from signal.h
 
 // Global Variables
 int myType;
-void *myVMStart;
+void *myVMStart; // start of memory
 int myVMSize;
 int myNumFrames;
 int myPageSize; // bytes in a page
 int myNumPages;
 int numFaults = 0;
+int numWriteBacks = 0;
 
 void mm_init(void *vm, int vm_size, int n_frames, int page_size , int policy)
 {
@@ -56,25 +57,26 @@ void mm_init(void *vm, int vm_size, int n_frames, int page_size , int policy)
 
 	//int mprotect(void *addr, size_t len, int prot);
 	mprotect(vm, vm_size, PROT_NONE); // PROT_NONE for right
+
+	sa.sa_flags = SA_RESTART; // restart functions if interrupted by handler
 	
 	//int sigemptyset(sigset_t *set);
 	sigset_t *set = &sa.sa_mask;
 	sigemptyset(set);
 
-	//now to connect it with 'mySigHandler(sa)'
+	//now bind it with 'mySigHandler' function
 	sa.sa_handler = mySigHandler(sa);
 
 	//int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact);
-	//sigaction( , , mySigHandler());
-	sigaction(SIGSEGV, &sa, NULL);
+	sigaction(SIGSEGV, &sa, NULL); // register signals
 }
 
 // Functions to Implement
-void mySigHandler(sa)
+void mySigHandler(int sigNum, siginfo_t st, void *unused)
 {
-	// sa.sa_addr == addr_1;
+	// st.si_addr == addr_1;
 	myPageSize = getpagesize(); // returns # of bytes in a page
-	int pageNumber = ( (int)sa.sa_addr - (int)myVMStart )/myPageSize;
+	nodee.pageNumber = ( ((int)st.si_addr - (int)myVMStart)/myPageSize ) + 1; // +1 to get to PAGE 1
 
 	// now have to check if its already in mem
 	  // if so, set PROT to Write
@@ -82,16 +84,31 @@ void mySigHandler(sa)
           // then gonna have to add memory
 	     // if memory is full then evict based on policy
 	     // if memory empty then add
-	if (pageNumber != NULL) // page is already in queue/memory
+	pageNode *walkList; // pointer to traverse through list
+
+	if (myType == FIFO)
 	{
-		mprotect(myVMStart, myVMSize, PROT_READ|PROT_WRITE);
+		while (walkList != NULL) // page is already in queue/memory
+		{
+			if (walkList > 0) // make sure its not empty
+			{//walkList->pageNumber == nodee.pageNumber
+				mprotect(myVMStart, myVMSize, PROT_READ|PROT_WRITE);
+				break;
+			}
+			walkList = walkList->next;
+		}
+		if (walkList == NULL) // its not in memory
+		{
+			numFaults++; // if not in mem, then page fault
+			mprotect(myVMStart, myVMSize, PROT_READ);
+		}
 	}
-	else // not in queue/memory
+/*	else // not in queue/memory
 	{
 		// So then add to queue/memory
 		numFaults = numFaults + 1;
-		mprotect(myVMStart, myVMSize, PROT_READ); // After its in queue, set privelage to READ.
-	}
+		mprotect(myVMStart, myVMSize, PROT_READ); // After its in queue, set permission to READ.
+	}*/
 }
 
 void FifoAlg (int index, int found)
@@ -111,8 +128,7 @@ void ClockAlg (int index, int found)
 
 unsigned long mm_nsigsegvs()
 {
-	int i;
-
+	
 	return 0l;
 }
 
@@ -133,19 +149,15 @@ int mm_report_nframe_evicts(int i)
 
 unsigned long mm_report_npage_faults()
 {
-/**
- * Fill Your Code Here
- *
- */
+	return numFaults;
+
 	return 0l;
 }
 
 unsigned long mm_report_nwrite_backs()
 {
-/**
- * Fill Your Code Here
- *
- */
+	return numWriteBacks;
+
 	return 0l;
 }
 
